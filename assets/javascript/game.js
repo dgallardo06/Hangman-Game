e654e8b7a63074087a6ef10ea827f22d4d528e16
dgallardo06 <denise.gallardo1@gmail.com>
2017-12-07 17:30:53 -0500

    logic for letters guessed

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,18 +8,28 @@ document.onkeyup = function (event){
 
 	var playerGuess = event.key;
 
-	var words = list[Math.floor(Math.random() * list.length)];
+	var words = list[Math.floor(Math.random() * list.length)]; //picks random word from list
+	var numChar = words.length; //determines how many letters in word
+  	console.log(numChar);
 
-	// var blanks = list.length;
 
 	//Display a blank space for each letter in word chosen
-	for (var i = 0; i < list.length; i++){
-		var blankDivs = document.createElement("div");
-		blankDivs = document.createTextNode("_ ");
-		word.append(blankDivs);
-	}
+	for (var i = 0; i < numChar; i++){
+		var blanks = document.createElement("div");
+		blanks = document.createTextNode("_ ");
+		//word.append(blanks);  ---this causes the blanks for each word to appear next to each other
+	} 
+
+	//document.getElementById("word").innerHTML = blanks; --- this causes for [object text] to appear instead of the lines
 
 	console.log(words);
 
+	//Add each guessed letter to list
+	guesses = guesses + " " + playerGuess;
+
+	//Show letters guessed
+	var remaining = document.getElementById("guessed");
+	remaining.innerHTML = guesses;
 
 }
+
